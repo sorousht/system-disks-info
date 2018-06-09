6dd20c9807b112a5d1c99f2b29fd4575c49ee3ab
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2018-06-09 23:30:32 +0430

    Print the install disks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate sysinfo;
+
+use sysinfo::SystemExt;
+use sysinfo::DiskExt;
+
+const KILOBYTE: u64 = 1024;
+const MEGABYTE: u64 = 1024 * 1024;
+const GIGABYTE: u64 = 1024 * 1024 * 1024;
+const TERABYTE: u64 = 1024 * 1024 * 1024 * 1024;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut system = sysinfo::System::new();
+    system.refresh_all();
+
+    for disk in system.get_disks() {
+        print!("{}({})",
+            disk.get_name(),
+            get_in_readable_unit(disk.get_total_space()));
+    }
+}
+
+fn get_in_readable_unit(value: u64) -> String {
+    if value < KILOBYTE {
+        return format!("{} Bytes", value);    
+    } else if value < MEGABYTE {
+        return format!("{} KB", value / KILOBYTE);
+    } else if value < GIGABYTE {
+        return format!("{} MB", value / MEGABYTE);
+    } else if value < TERABYTE {
+        return format!("{} GB", value / GIGABYTE);
+    } else {
+        return format!("{} TB", value / TERABYTE);
+    }
 }
